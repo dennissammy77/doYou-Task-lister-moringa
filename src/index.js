81f17634a0e7515df5e8becea8031696805b7de8
dennissammy77 <dennissammy77@gmail.com>
2025-03-15 22:31:15 +0300

    building up on the aside section; profile; task creation form

--- src/index.js
+++ src/index.js
@@ -113,4 +113,37 @@ document.addEventListener("DOMContentLoaded", function () {
             loadMoreTasks();
         }
     });
+
+    const asideContainer = document.querySelector(".aside-container");
+    const createTaskBtn = document.getElementById("createTask");
+
+    createTaskBtn.addEventListener("click", function () {
+        asideContainer.classList.toggle("flipped");
+    });
+    const closeTaskFormBtn = document.getElementById("closeTaskForm");
+
+    closeTaskFormBtn.addEventListener("click", function () {
+        asideContainer.classList.toggle("flipped");
+    });
+
+    // create task Form
+    document.getElementById("createTaskForm").addEventListener("submit",(event)=>{
+        event.preventDefault();
+        // validate payload
+        try {
+            const name = document.getElementById("taskName").value;
+            const description = document.getElementById("taskDescription").value;
+            const priority = document.getElementById("taskPriority").value;
+            const date = document.getElementById("taskDate").value;
+            if(!name || !description || !priority || !date){
+                alert("ensure all inputs are field");
+                return
+            }
+            const data = {name, description, priority,date};
+            // store in local storage or indexDb
+        } catch (error) {
+            console.log(error);
+        }
+        
+    })
 });
